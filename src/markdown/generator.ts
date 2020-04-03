30b2719c2a42ddab3a58d35be512fd3870a43a97
Eyas Sharaiha <eyas.sharaiha@gmail.com>
2020-04-02 21:40:12 -0400

    Nicer imports.

--- src/markdown/generator.ts
+++ src/markdown/generator.ts
@@ -1,8 +1,8 @@
-import * as r from "../core/resume";
 import { RenderDateRange } from "../render/render";
+import { Resume, Category, EntityInvolvements, Involvement, SortEntitiesDescending } from "../core/resume";
 
-export function toMarkdown(resume: r.Resume): string {
-    resume.categories.forEach(category => r.SortEntitiesDescending(category.entities));
+export function toMarkdown(resume: Resume): string {
+    resume.categories.forEach(category => SortEntitiesDescending(category.entities));
 
     var file: string = "";
     file += resume.person.name + "\n" + Array(resume.person.name.length + 1).join('=') + "\n\n";
@@ -11,13 +11,13 @@ export function toMarkdown(resume: r.Resume): string {
     return file;
 }
 
-function categoryToMarkdown(category: r.Category): string {
+function categoryToMarkdown(category: Category): string {
     var str = category.name + "\n" + Array(category.name.length + 1).join('-') + "\n\n";
     str += category.entities.map(entityToMarkdown).join("\n\n");
     return str;
 }
 
-function entityToMarkdown(entity: r.EntityInvolvements): string {
+function entityToMarkdown(entity: EntityInvolvements): string {
     var str = "## " + entity.entity + "\n";
     if (entity.location) str += entity.location + "\n";
     if (entity.entityDescription) str += entity.entityDescription + "\n";
@@ -26,7 +26,7 @@ function entityToMarkdown(entity: r.EntityInvolvements): string {
     return str;
 }
 
-function involvementToMarkdown(involvement: r.Involvement): string {
+function involvementToMarkdown(involvement: Involvement): string {
     var str = "### " + involvement.title + "\n" + RenderDateRange(involvement.dates);
     if (involvement.description || involvement.properties) {
         str += "\n";
